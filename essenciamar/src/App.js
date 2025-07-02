f9e4dd98120a93e47810fc0684486f17c7659539
isaa <isaa>
2025-07-02 18:21:55 -0300

    dewcricao

---
+++ essenciamar/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Principal from './pages/Principal/principal';
+
+function App() {
+  return (
+    <Principal />
+  );
+}
+
+export default App;
---
+++ essenciamar/src/pages/Login/login.js
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+  <meta charset="UTF-8">
+  <title>Essência Do Mar</title>
+  <link rel="stylesheet" href="style2.css"> 
+</head>
+<body>
+    <div>
+        <img src="logo.jpg" alt="Descrição da imagem" class="img">
+    </div>
+    <div class="input-container">
+        <div class="input-box">
+            <label for="input1">Input 1</label>
+            <input type="text" id="input1" />
+        </div>
+        <div class="input-box">
+            <label for="input2">Input 2</label>
+            <input type="text" id="input2" />
+        </div>
+    </div>
+    <h1>
+        <a href="login.html" class="link">Entrar</a>
+    </h1>
+    <div>
+        <img src="orlga.jpg" alt="Descrição da imagem" class="img2">
+    </div>
+    <div>
+        <img src="orlga.jpg" alt="Descrição da imagem" class="img3">
+    </div>
+
+   
+      
+
+  
+
+  <script>
+  </script>
+
+</body>
+</html>
+
